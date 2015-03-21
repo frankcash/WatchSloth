ac002f93b15e5459c5513900dfcac332bf5e2a56
Frank Cash <cfcash@mtu.edu>
2015-03-21 08:12:09 -0400

    Update config and app.js
    App.js now uploads image to s3 and yo's it successfully

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var tessel = require('tessel'); // import tessel
 var Q = require('q');
 var Yo = require('yo-api');
 var config = require('./config/config');
+var uploadImage = require('tessel-camera-s3');
 var camera = require('camera-vc0706').use(tessel.port['A']);
 var gpio = tessel.port['GPIO']; // select the GPIO port
 
@@ -11,6 +12,25 @@ myPin.output(1);
 
 yo = new Yo(config.yo); // Initializes yo object
 
+function makeRand(len){
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < len; i++ )
+  text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+var s3Config = {
+  key:config.key,
+  secret:config.secret,
+  bucket:config.bucket
+};
+
+console.log(s3Config);
+
+var baseS3 = "https://s3.amazonaws.com/slothwatch/"
 
 camera.on('ready', function() {
   console.log("On Ready");
@@ -27,7 +47,7 @@ camera.on('ready', function() {
           console.log("f");
         }
       );
-      yo.yo("frankcash");
+      // yo.yo("frankcash");
 
     }else{
       console.log('Reading pin:', myPin.rawRead());
@@ -52,9 +72,19 @@ function takePic(){
         console.log('error taking image', err);
       }else{
         notificationLED.low();
-        var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
+        var name = 'picture-' + makeRand(4) + '.jpg';
         console.log('Picture saving as', name, '...');
         process.sendfile(name, image);
+
+        uploadImage(image, name, s3Config, function(err, res) {
+          if (err) {
+            return console.log('There was an error :( ');
+          }
+          console.log('image was successfully uploaded!');
+          // name = name.replace("\+", "%2b");
+          yo.yo_link("frankcash", baseS3+name);
+        });
+
         console.log('done.');
         // camera.disable();
        }
--- config/config.sample.js
+++ config/config.sample.js
@@ -1,5 +1,12 @@
 var config = {}
 
+// Yo API
 config.yo = ""; // put your API token here
+
+
+// Amazon S3 Access
+config.key ='';
+config.secret ='';
+config.bucket ='';
 
 module.exports = config;
